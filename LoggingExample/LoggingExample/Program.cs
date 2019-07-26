1027ba7594e5701e6486507c7a9cbeffe62879aa
Paulius J <paulius.juozelskis@gmail.com>
2019-07-26 21:20:24 +0300

    Centralized logging implementation

---
+++ LoggingExample/LoggingExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace LoggingExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .ConfigureLogging(loggingConfiguration => loggingConfiguration.ClearProviders())
+            .UseSerilog((hostingContext, loggerConfiguration) => 
+                loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
+    }
+}
